c61ca4b6d863c5d3db8ef6e508f25e99199cdc7c
otsen <otsen@DESKTOP-I5A52VV.localdomain>
2020-11-01 12:21:12 -0800

    Load dataframe

--- stock_classifier.py
+++ stock_classifier.py
@@ -1,7 +1,18 @@
+import os
+import pandas as pd
+
+
 def main():
+    DATA_ROOT = 'Data.csv'
+
     # Load data 
+    if not os.path.exists(DATA_ROOT):
+        raise OSError("[ERROR]: Data root directory does not exist.")
+    
+    df = pd.read_csv(DATA_ROOT, encoding="ISO-8859-1")
+    print("[INFO]: Data successfully loaded as data frame.")
     # Preprocess data
-    pass
+    # Fit data into count vectorizer
 
 if __name__ == "__main__":
     main()
