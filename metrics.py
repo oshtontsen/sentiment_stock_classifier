7da49d3980728f1650bcaa1c7d5a016329b0c140
otsen <otsen@DESKTOP-I5A52VV.localdomain>
2020-11-03 20:19:58 -0800

    Add metrics

---
+++ metrics.py
@@ -0,0 +1,15 @@
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score
+
+class Metrics():
+    def __init__(self, predictions, test_labels):
+        self.predictions = predictions
+        self.test_labels = test_labels
+        self.cfn_matrix = self._get_confusion_matrix()
+
+    def _get_confusion_matrix(self):
+        return confusion_matrix(self.test_labels, self.predictions)
+
+    def print_confusion_matrix(self):
+        print(sns.heatmap(self.cfn_matrix, annot=True))
--- stock_sentiment_classifier.py
+++ stock_sentiment_classifier.py
@@ -1,28 +1,29 @@
 import os
 import ss_baseline
 import ss_randomizer 
+import metrics
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
 class BaselineClassifier():
-    def __init__(self, n_estimators):
-        self.countvector = self._get_countvector()
+    def __init__(self, n_estimators, ngram_range):
+        self.countvector = self._get_countvectorizer(ngram_range)
         self.ss_classifier = RandomForestClassifier(n_estimators=200, criterion='entropy')
 
-    def _get_countvectorizer(self):
-        return Countvectorizer(ngram_range(ngram_range[0], ngram_range[1]))
+    def _get_countvectorizer(self, ngram_range):
+        return CountVectorizer(ngram_range=(ngram_range[0], ngram_range[1]))
 
-def build_graph(n_estimators):
-    return BaselineClassifier(n_estimators)
+def build_graph(n_estimators, ngram_range):
+    return BaselineClassifier(n_estimators, ngram_range)
 
-def train_step():
-    pass
+def build_metrics(predictions, test_labels):
+    return Metrics(predictions, test_labels)
 
 
 def main():
     DATA_ROOT = 'Data.csv'
-    N_GRAM_RANGE = (2, 2)
+    NGRAM_RANGE = (2, 2)
     N_ESTIMATORS = 200
     SPLIT_IDX = '2014-12-31'
 
@@ -34,17 +35,22 @@ def main():
     data_randomizer = ss_randomizer.setup_randomforest_randomizer(DATA_ROOT, SPLIT_IDX) 
 
     # Fit data into count vectorizer
-    model = build_graph(N_ESTIMATORS)
+    model = build_graph(N_ESTIMATORS, NGRAM_RANGE)
+    print("[INFO]: Successfully built new classifier graph")
     X_train = model.countvector.fit_transform(data_randomizer.train_features)
-    X_test = model.countvector.fit_transform(data_randomizer.test_features)
+    X_test = model.countvector.transform(data_randomizer.test_features)
 
     # Train the classifier
+    print("[INFO]: Beginning RandomForest training")
     model.ss_classifier.fit(X_train, data_randomizer.train_labels)
+    print("[INFO]: Successfully completed RandomForest training")
 
     # Perform inference
     predictions = model.ss_classifier.predict(X_test)
 
     # Define the metrics object
+    metrics = build_metrics(predictions, data_randomizer.test_labels)
+    metrics.print_confusion_matrix()
 
 
 if __name__ == "__main__":
