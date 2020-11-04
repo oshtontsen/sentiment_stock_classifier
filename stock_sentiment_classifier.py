4fd26b9f2329536e5e7b9cf6a060c69a372b1cab
otsen <otsen@DESKTOP-I5A52VV.localdomain>
2020-11-03 20:34:13 -0800

    Rename metrics file

--- stock_sentiment_classifier.py
+++ stock_sentiment_classifier.py
@@ -1,25 +1,7 @@
 import os
 import ss_baseline
 import ss_randomizer 
-import metrics
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.ensemble import RandomForestClassifier
-
-class BaselineClassifier():
-    def __init__(self, n_estimators, ngram_range):
-        self.countvector = self._get_countvectorizer(ngram_range)
-        self.ss_classifier = RandomForestClassifier(n_estimators=200, criterion='entropy')
-
-    def _get_countvectorizer(self, ngram_range):
-        return CountVectorizer(ngram_range=(ngram_range[0], ngram_range[1]))
-
-def build_graph(n_estimators, ngram_range):
-    return BaselineClassifier(n_estimators, ngram_range)
-
-def build_metrics(predictions, test_labels):
-    return Metrics(predictions, test_labels)
-
+import validation_metrics
 
 def main():
     DATA_ROOT = 'Data.csv'
@@ -49,7 +31,7 @@ def main():
     predictions = model.ss_classifier.predict(X_test)
 
     # Define the metrics object
-    metrics = build_metrics(predictions, data_randomizer.test_labels)
+    metrics = vaildation_metrics.build_metrics(predictions, data_randomizer.test_labels)
     metrics.print_confusion_matrix()
 
 
